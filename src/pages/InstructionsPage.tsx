5103b53f94926c86deac2d89f1cee5fca1af8e4d
Hamza Faheem <hamza.faheem@zekiexperts.com>
2026-03-13 04:09:56 +0500

    arrowdirection

--- src/pages/InstructionsPage.tsx
+++ src/pages/InstructionsPage.tsx
@@ -45,8 +45,8 @@ export default function InstructionsPage() {
                             >
                                 <span className="button-icon-circle">
                                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                        <line x1="5" y1="12" x2="19" y2="12"></line>
-                                        <polyline points="12 5 19 12 12 19"></polyline>
+                                        <line x1="19" y1="12" x2="5" y2="12"></line>
+                                        <polyline points="12 19 5 12 12 5"></polyline>
                                     </svg>
                                 </span>
                                 فارم پُر کریں
